aee1ca1e2c360fc14078afbd13eb2988eb84e443
Sabirbinsakander <97218163+Sabirbinsakander@users.noreply.github.com>
2024-09-08 18:21:13 +0600

    first commit

---
+++ script/json_index.js
@@ -0,0 +1,29 @@
+// const data = require ("./delete_json.json");
+// // console.log(data);
+
+// // console.log(data.friends[0]);
+// // data.friends[0].name="Raisa";
+// // console.log(data.friends[0].name);
+// delete data.friends[0].name;
+// console.log(data);
+
+// const data = require("./students_data.json")
+// console.log(data.students[1].language[0]);
+
+// const data = require("./friends_data.json")
+
+
+// for(x in data)
+//     {
+//         console.log(x);
+//         console.log(data[x]);
+//     }
+
+const data2 ={
+    name :"anis",
+    age :25
+
+}
+// console.log(JSON.stringify(data2));
+
+console.log(JSON.stringify('{"name" : "anis"}'));
